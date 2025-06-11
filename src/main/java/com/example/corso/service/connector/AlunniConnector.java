41de3769d8209cff0ce29d127681e034074b21c3
MariniESH <a.marini@elitesoftwarehouse.com>
2025-06-11 10:44:13 +0200

    aggiunta autenticazione nelle headers dei connectors

--- src/main/java/com/example/corso/service/connector/AlunniConnector.java
+++ src/main/java/com/example/corso/service/connector/AlunniConnector.java
@@ -2,13 +2,17 @@ package com.example.corso.service.connector;
 
 import com.example.corso.dto.AlunnoWithoutCorsiDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Base64;
 import java.util.List;
 
 @Component
 public class AlunniConnector {
+    private String credentials = "user:pass1234";
+    private String encodedAuth = Base64.getEncoder().encodeToString(credentials.getBytes());
 
     @Autowired
     WebClient webClient;
@@ -16,6 +20,7 @@ public class AlunniConnector {
     public AlunnoWithoutCorsiDTO getAlunno(Long id) {
         return webClient.get()
                 .uri("/alunni/{id}", id)
+                .header(HttpHeaders.AUTHORIZATION, "Basic " + encodedAuth)
                 .retrieve()
                 .bodyToMono(AlunnoWithoutCorsiDTO.class)
                 .block();
--- src/main/java/com/example/corso/service/connector/DocenteConnector.java
+++ src/main/java/com/example/corso/service/connector/DocenteConnector.java
@@ -3,11 +3,16 @@ package com.example.corso.service.connector;
 import com.example.corso.dto.DocenteDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Base64;
+
 @Component
 public class DocenteConnector {
+    private String credentials = "user:pass1234";
+    private String encodedAuth = Base64.getEncoder().encodeToString(credentials.getBytes());
 
     @Autowired
     WebClient webClient;
@@ -15,6 +20,7 @@ public class DocenteConnector {
     public DocenteDTO getDocente(Long id) {
         return webClient.get()
                 .uri("/docenti/{id}", id)
+                .header(HttpHeaders.AUTHORIZATION, "Basic " + encodedAuth)
                 .retrieve()
                 .bodyToMono(DocenteDTO.class)
                 .block();
